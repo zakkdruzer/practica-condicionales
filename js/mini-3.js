38883bb38dfb854b627957731d91179174ba2e38
zakkdruzer <zakkdruzer@gmail.com>
2026-06-17 23:29:49 -0300

    feat: mini 3 - completado

---
+++ js/mini-3.js
@@ -0,0 +1,57 @@
+// Dato de entrada (prueba con varios de la tabla):
+let temperatura1 = 20;
+let temperatura2 = 10;
+let temperatura3 = 30;
+let temperatura4 = 40;
+
+console.log("** Mini 3 · Clasificador de temperatura **");
+
+// 👇 Escribe tu cadena de if / else if / else
+if (temperatura1 < 5) {
+  console.log("Helado 🥶");
+} else if (temperatura1 < 15) {
+  console.log("Frío 🧥");
+} else if (temperatura1 < 25) {
+  console.log("Templado 😎");
+} else if (temperatura1 < 35) {
+  console.log("Caluroso 🥵");
+} else {
+  console.log("Infierno 🔥");
+}
+// Casos distintos
+
+if (temperatura2 < 5) {
+  console.log("Helado 🥶");
+} else if (temperatura2 < 15) {
+  console.log("Frío 🧥");
+} else if (temperatura2 < 25) {
+  console.log("Templado 😎");
+} else if (temperatura2 < 35) {
+  console.log("Caluroso 🥵");
+} else {
+  console.log("Infierno 🔥");
+}
+
+if (temperatura3 < 5) {
+  console.log("Helado 🥶");
+} else if (temperatura3 < 15) {
+  console.log("Frío 🧥");
+} else if (temperatura3 < 25) {
+  console.log("Templado 😎");
+} else if (temperatura3 < 35) {
+  console.log("Caluroso 🥵");
+} else {
+  console.log("Infierno 🔥");
+}
+
+if (temperatura4 < 5) {
+  console.log("Helado 🥶");
+} else if (temperatura4 < 15) {
+  console.log("Frío 🧥");
+} else if (temperatura4 < 25) {
+  console.log("Templado 😎");
+} else if (temperatura4 < 35) {
+  console.log("Caluroso 🥵");
+} else {
+  console.log("Infierno 🔥");
+}
